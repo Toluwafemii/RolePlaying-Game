1d016df3b1c0f068a24482114b5576f8dafc3377
Your Name <davidokaseun@gmail.com>
2024-12-07 10:12:02 +0100

    defeat monster function added

--- main.js
+++ main.js
@@ -176,3 +176,11 @@ function attack() {
 function dodge() {
 
 }
+
+function defeatMonster() {
+    gold += Math.floor(monsters[fighting].level * 6.7);
+    xp += monsters[fighting].level;
+    goldText.innerText = gold;
+    xpText.innerText = xp;
+    update(locations[4]);
+  }
